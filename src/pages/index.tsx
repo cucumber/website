f564d207932119762b2739b79b52bd5e627919e1
David Goss <david@davidgoss.co>
2023-10-29 17:17:17 +0000

    display a row of logos

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -63,6 +63,16 @@ export default function Home() {
                         </div>
                     </div>
                 </div>
+                <div className="container padding-vert--lg">
+                    <ul className={styles.logosList}>
+                        <li><img alt="Brithtcove" src="/img/users/brightcove.svg"/></li>
+                        <li><img alt="EXL" src="/img/users/exl.png"/></li>
+                        <li><img alt="FinanceIt" src="/img/users/financeit.svg"/></li>
+                        <li><img alt="Pendo" src="/img/users/pendo.svg"/></li>
+                        <li><img alt="SproutSocial" src="/img/users/sproutsocial.svg"/></li>
+                        <li><img alt="Trivago" src="/img/users/trivago.svg"/></li>
+                    </ul>
+                </div>
             </main>
         </Layout>
     );
