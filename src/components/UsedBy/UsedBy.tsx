a59bcf7f07769745f62b59060c2ae4b79f59fa63
David Goss <david@davidgoss.co>
2023-11-07 21:06:58 +0000

    refactor, add strapline, make responsive

--- src/components/UsedBy/UsedBy.tsx
+++ src/components/UsedBy/UsedBy.tsx
@@ -1,13 +1,24 @@
 import React, {FC} from "react";
 import styles from './UsedBy.module.scss';
 
+const LOGOS = [
+    ['Brightcove', 'brightcove.svg'],
+    ['EXL', 'exl.png'],
+    ['FinanceIt', 'financeit.svg'],
+    ['Pendo', 'pendo.svg'],
+    ['SproutSocial', 'sproutsocial.svg'],
+    ['Trivago', 'trivago.svg'],
+]
+
 export const UsedBy: FC = () => {
-    return <ul className={styles.logosList}>
-        <li><img alt="Brithtcove" src="/img/users/brightcove.svg"/></li>
-        <li><img alt="EXL" src="/img/users/exl.png"/></li>
-        <li><img alt="FinanceIt" src="/img/users/financeit.svg"/></li>
-        <li><img alt="Pendo" src="/img/users/pendo.svg"/></li>
-        <li><img alt="SproutSocial" src="/img/users/sproutsocial.svg"/></li>
-        <li><img alt="Trivago" src="/img/users/trivago.svg"/></li>
-    </ul>
+    return <>
+        <p className={styles.strapLine}>Loved by teams everywhere</p>
+        <ul className={styles.logosList}>
+            {LOGOS.map(([alt, filename]) => (
+                <li>
+                    <img alt={alt} src={'/img/users/' + filename}/>
+                </li>
+            ))}
+        </ul>
+    </>
 }
