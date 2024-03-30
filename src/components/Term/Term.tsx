a67d258c8c5159aa9d32ad0839105d8a5d44aa94
David Goss <david@davidgoss.co>
2024-03-30 11:01:39 +0000

    support getting from localStorage as fallback

--- src/components/Term/Term.tsx
+++ src/components/Term/Term.tsx
@@ -1,9 +1,9 @@
 import {FC} from "react";
 import terms from './terms.json'
-import { useHistory } from "@docusaurus/router";
+import {useHistory} from "@docusaurus/router";
 
-export const Term: FC<{children: keyof typeof terms}> = ({children}) => {
+export const Term: FC<{ children: keyof typeof terms }> = ({children}) => {
     const history = useHistory()
-    const lang = new URLSearchParams(history.location.search).get('lang') || 'java'
+    const lang = new URLSearchParams(history.location.search).get('lang') || localStorage.getItem('docusaurus.tab.lang') || 'java'
     return <span>{terms[children][lang]}</span>
 }
