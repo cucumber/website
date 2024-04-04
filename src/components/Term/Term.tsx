d8cd69777ee21ed0eee66767b5c3a411c73644cd
David Goss <david@davidgoss.co>
2024-04-04 19:05:56 +0100

    add BrowserOnly wrapper

--- src/components/Term/Term.tsx
+++ src/components/Term/Term.tsx
@@ -1,9 +1,22 @@
 import {FC} from "react";
 import terms from './terms.json'
 import {useHistory} from "@docusaurus/router";
+import BrowserOnly from '@docusaurus/BrowserOnly';
 
-export const Term: FC<{ children: keyof typeof terms }> = ({children}) => {
+type TermProps = { children: keyof typeof terms };
+
+const DEFAULT_LANG = 'java';
+
+export const InternalTerm: FC<TermProps> = ({children}) => {
     const history = useHistory()
-    const lang = new URLSearchParams(history.location.search).get('lang') || localStorage.getItem('docusaurus.tab.lang') || 'java'
+    const lang = new URLSearchParams(history.location.search).get('lang') || localStorage.getItem('docusaurus.tab.lang') || DEFAULT_LANG
     return <span>{terms[children][lang]}</span>
+}
+
+export const Term: FC<TermProps> = ({children}) => {
+    return <BrowserOnly fallback={<span>{terms[children][DEFAULT_LANG]}</span>}>
+        {() => {
+            return <InternalTerm>{children}</InternalTerm>
+        }}
+    </BrowserOnly>
 }
