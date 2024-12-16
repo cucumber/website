5680abafb50120bbd9a8f75bc96bb014ee0c2f16
David Goss <david@davidgoss.co>
2024-12-16 23:40:28 +0000

    tweak form styling

--- src/components/Newsletter/Subscribe.tsx
+++ src/components/Newsletter/Subscribe.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react'
 import styles from './Subscribe.module.scss'
+import clsx from 'clsx'
 
 export const Subscribe: FC = () => {
   return <>
@@ -8,11 +9,12 @@ export const Subscribe: FC = () => {
       action="https://buttondown.com/api/emails/embed-subscribe/cucumber"
       method="post"
       target="_blank"
+      className={styles.form}
     >
       <input type="email" name="email" placeholder="you@example.com" required className={styles.field} />
       <input type="hidden" name="embed" value="1" />
       <input type="hidden" name="tag" value="cucumber.io" />
-      <button className="button button--primary" type="submit">Subscribe</button>
+      <button type="submit" className={clsx('button', 'button--primary', styles.submit)}>Subscribe</button>
     </form>
   </>
 }
