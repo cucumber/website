0c00845d146a3fdb2bbf3c4927902dab1f3f12ef
David Goss <david@davidgoss.co>
2025-05-09 10:04:19 +0100

    use effect to (re)load ads

--- src/components/Ads/ImageAd.tsx
+++ src/components/Ads/ImageAd.tsx
@@ -1,5 +1,6 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect } from 'react'
 
 export const ImageAd: FC = () => {
+  useEffect(() => window.ethicalads.reload(), [])
   return <div data-ea-publisher="cucumberio" data-ea-type="image"></div>
 }
