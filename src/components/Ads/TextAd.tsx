ba72ba335267c6f4e961c6088b7d1c4b7da14dcc
David Goss <david@davidgoss.co>
2025-05-09 17:55:05 +0100

    refactor internals

--- src/components/Ads/TextAd.tsx
+++ src/components/Ads/TextAd.tsx
@@ -1,17 +1,7 @@
-import React, { FC, useEffect } from 'react'
+import React, { FC } from 'react'
 import { AdTarget } from './types'
-import { useWindowSize } from '@docusaurus/theme-common'
+import { Ad } from './Ad'
 
 export const TextAd: FC<{ target: AdTarget }> = ({ target }) => {
-  const windowSize = useWindowSize()
-  const show = windowSize === target
-  useEffect(() => {
-    if (show) {
-      window.ethicalads.reload()
-    }
-  }, [show])
-  if (!show) {
-    return null
-  }
-  return <div className="bordered" data-ea-publisher="cucumberio" data-ea-type="text"></div>
+  return <Ad target={target} type="text" />
 }
