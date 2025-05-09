7545c01ca1b7cdfeae5602866d488eabbcbe41bf
David Goss <david@davidgoss.co>
2025-05-09 12:45:23 +0100

    show in more places

---
+++ src/theme/BlogLayout/index.tsx
@@ -0,0 +1,27 @@
+import React, { type ReactNode } from 'react'
+import BlogLayout from '@theme-original/BlogLayout'
+import type BlogLayoutType from '@theme/BlogLayout'
+import type { WrapperProps } from '@docusaurus/types'
+import { ImageAd } from '@site/src/components/Ads'
+
+type Props = WrapperProps<typeof BlogLayoutType>
+
+/*
+ * Render an ad in the TOC area (right side on desktop) for all blog routes, including
+ * archives, tags, authors and posts themselves.
+ */
+export default function BlogLayoutWrapper(props: Props): ReactNode {
+  return (
+    <>
+      <BlogLayout
+        {...props}
+        toc={
+          <>
+            <ImageAd />
+            {props.toc}
+          </>
+        }
+      />
+    </>
+  )
+}
