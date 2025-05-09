11b58c6d7b33c7810e6534436fa82adadfa47e84
David Goss <david@davidgoss.co>
2025-05-09 10:26:44 +0100

    use bordered ad style

--- src/components/Ads/ImageAd.tsx
+++ src/components/Ads/ImageAd.tsx
@@ -2,5 +2,5 @@ import React, { FC, useEffect } from 'react'
 
 export const ImageAd: FC = () => {
   useEffect(() => window.ethicalads.reload(), [])
-  return <div data-ea-publisher="cucumberio" data-ea-type="image"></div>
+  return <div className="bordered" data-ea-publisher="cucumberio" data-ea-type="image"></div>
 }
