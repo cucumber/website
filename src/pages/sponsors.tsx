9edefc9d184116e949c27bd0bb237ebb075cdfd6
David Goss <david@davidgoss.co>
2026-03-26 18:55:18 +0000

    prettier:write

--- src/pages/sponsors.tsx
+++ src/pages/sponsors.tsx
@@ -109,11 +109,7 @@ const SponsorsList: FC<SponsorsListProps> = ({ sponsors, size = 'lg' }) => {
     <ul className={styles.list}>
       {sponsors.map((sponsor) => (
         <li key={sponsor.name} className="margin-bottom--md">
-          <Link
-            href={sponsor.url}
-            rel="noopener noreferrer"
-            className={`avatar ${styles.sponsor}`}
-          >
+          <Link href={sponsor.url} rel="noopener noreferrer" className={`avatar ${styles.sponsor}`}>
             <img
               className={`avatar__photo avatar__photo--${size} ${styles.sponsorLogo}`}
               src={sponsor.logo}
